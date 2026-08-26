71d794f4021d0c86f36dfc1486d65889ae094d13
DipTaken <dennisqin61@gmail.com>
2026-08-25 18:58:26 -0700

    landing page now updates with login

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,21 @@ import LoginButton from '@/app/components/LoginButton'
 
 export default async function LandingPage() {
     const cookieStore = await cookies()
-    //const supabase = createClient(cookieStore)
-    //const guestId = await getGuestIdReadOnly()
-    
+    const supabase = createClient(cookieStore)
+    const guestId = await getGuestIdReadOnly()
+    const { data: { user } } = await supabase.auth.getUser()
+
     return (
         <div className="flex flex-col items-center justify-center gap-10 py-10">
             <h1 className="text-3xl font-bold">Arcaea B50 Web</h1>
-            <h2 className="text-xl font-light">Login to begin</h2>
-            <div>
-                <LoginButton/>
-            </div>
+            {user ? (
+                <span>Welcome, {user.email?.split('@')[0]}</span>
+            ) : (
+                <div className="flex flex-col items-center gap-4">
+                    <h2 className="text-xl font-light">Login to begin</h2>
+                    <LoginButton/>
+                </div>
+            )}
         </div>
     )
 }
