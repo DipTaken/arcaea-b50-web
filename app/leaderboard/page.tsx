4c3b45d4f2d25cb9a1ca1180a8e7e914391adbd6
DipTaken <dennisqin61@gmail.com>
2026-08-25 18:44:24 -0700

    Created leaderboard page

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+export default async function LeaderboardPage() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-10 py-10">
+            <h1 className="text-3xl font-bold">Leaderboard</h1>
+        </div>
+    )
+}
