c117a49b96f43846411980ea2a3d065786e0a189
DipTaken <dennisqin61@gmail.com>
2026-09-01 22:37:18 -0700

    feat: added changelog to main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 import LoginButton from '@/app/components/LoginButton'
 import { PageShell } from './components/PageShell'
+import { Panel } from './components/Panel'
 
 export default async function LandingPage() {
     const cookieStore = await cookies()
@@ -9,10 +10,10 @@ export default async function LandingPage() {
     const { data: { user } } = await supabase.auth.getUser()
 
     return (
-        <PageShell 
-            title="Arcaea B50 Web" 
+        <PageShell
+            title="Arcaea B50 Web"
             subtitle="">
-           
+
             {/* Display a welcome message if the user is logged in, or a login prompt if not */}
             {user && !user.is_anonymous ? (
                 <span>Welcome, {user.email?.split('@')[0]}</span>
@@ -22,6 +23,24 @@ export default async function LandingPage() {
                     <LoginButton />
                 </div>
             )}
+            
+            
+            {/*CHANGELOG*/}
+            <Panel>
+                <div className="flex flex-col gap-6 p-4">
+                    <h1>
+                        <span className="text-4xl font-bold text-white text-center">Changelog</span>
+                    </h1>
+
+                    {/*THIS SHOULD BE REVERSE ORDER*/}
+                    <p className="text-white">
+                        <span className="font-bold">v1.1.0</span> - Added score import functionality. Users can now import scores from CSV files.
+                    </p>
+                    <p className="text-white">
+                        <span className="font-bold">v1.0.0</span> - Initial release of Arcaea B50 Web. Users can log in, and view, add, edit, and delete their scores.
+                    </p>
+                </div>
+            </Panel>
         </PageShell>
     )
 }
